473f805d227c7da3af40b045eadf64520f1975b9
lea <monthieuxlea@gmail.com>
2024-10-28 16:34:32 +0100

    lea

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from './component/Footer.tsx' // 1°fonction déclaré ici qui et donc apres tranformé en balise
+import {useState} from 'react' // importer la fonctionnalité usestate dans le projet DEPUIS  react. 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countLait, setCountLait] = useState(0); //2° usestate informations qui doivent circuler en app et la fonction footer en forme de balise
+  const [countPot, setCountPot] = useState(0);
+return (
+  <section className="app">
+    
+     <Footer
+     countLait = {countLait} // 3°chffres compte 
+     setCountLait = {setCountLait} //fonction qui change le compte, incrémente
+     countPot = {countPot}
+     setCountPot = {setCountPot}
+     /> 
+     {/* 4°remettre ce qu'on a declaré dans app au footer*/} 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  </section>
+ 
+)
+
 }
+
 
 export default App
