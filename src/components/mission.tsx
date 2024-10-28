757c489d6b1d4c6fe36ed709bafe7e05bd8bfcf8
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-28 14:51:44 +0100

    fin mission

---
+++ src/components/mission.tsx
@@ -0,0 +1,33 @@
+import "./mission.css";
+
+export default function Mission() {
+  const obj1 = Math.floor(Math.random() * (10 - 0) + 0);
+  const obj2 = Math.floor(Math.random() * (20 - 0) + 0);
+  const punishment = [
+    "To be spanked(lightly).",
+    "To wear their nappies (like a little baby).",
+    "To have no candies for a week.",
+  ];
+  const i = punishment.length - 1;
+  const punish = Math.floor(Math.random() * (i - 0) + 0);
+  return (
+    <div className="aside">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap');
+      </style>
+      <h1>Missions</h1>
+      <section>
+        <h2>Objectifs</h2>
+        <ul className="objectifs">
+          <li>Will have to drink {obj1} mini bibs.</li>
+          <li>Will have to eat {obj2} mini veggie pots.</li>
+        </ul>
+      </section>
+      <section>
+        <h2>If mistakes are made they will have :</h2>
+        <p>{punishment[punish]}</p>
+      </section>
+    </div>
+  );
+}
